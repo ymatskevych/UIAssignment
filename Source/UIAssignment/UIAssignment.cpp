a3fb32f06ee8be9efb5a6fff17f2866d578a135f
Yurii Matskevych <matskevych.yurii@gmail.com>
2023-10-09 16:33:25 +0300

    init comming

---
+++ Source/UIAssignment/UIAssignment.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UIAssignment.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UIAssignment, "UIAssignment" );
---
+++ Source/UIAssignment/UIAssignmentGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UIAssignmentGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UIASSIGNMENT_API AUIAssignmentGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
